2fdb4a7740e0a68f15a13c07d681502e59dd899f
yohunl <yohunl@163.com>
2016-01-22 01:00:29 +0800

    Initial Commit

---
+++ AOPProxy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AOPProxy
+//
+//  Created by yohunl on 16/1/22.
+//  Copyright © 2016年 yohunl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
